07a9232703037c54dfce6911b932ff1ccb6a2c92
赵鹏 <zhangsan@itcast.cn>
2019-11-12 20:01:15 +0800

    1213

---
+++ fp/hh/src/cn/itcast/Champing.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+public class Champing {
+    public static void main(String[] args) {
+        System.out.println(1212);
+        System.out.println(1212);
+        System.out.println(1212);
+        System.out.println(1212);
+        System.out.println(1212);
+        System.out.println(1212);
+    }
+}
